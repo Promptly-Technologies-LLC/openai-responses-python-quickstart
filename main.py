c05b63995955c11ea2a7b97f727a351e354a538d
Christopher C. Smith <chriscarrollsmith@gmail.com>
2025-02-22 16:27:45 -0500

    Minor bugfixes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
-from routers import chat, files, api_keys, assistants
+from routers import chat, files, setup
 from utils.threads import create_thread
 from fastapi.exceptions import HTTPException
 
@@ -24,8 +24,7 @@ app = FastAPI(lifespan=lifespan)
 # Mount routers
 app.include_router(chat.router)
 app.include_router(files.router)
-app.include_router(api_keys.router)
-app.include_router(assistants.router)
+app.include_router(setup.router)
 
 # Mount static files (e.g., CSS, JS)
 app.mount("/static", StaticFiles(directory=os.path.join(os.getcwd(), "static")), name="static")
@@ -61,7 +60,7 @@ async def read_home(request: Request, thread_id: str = None, messages: list = []
     # Check if environment variables are missing
     load_dotenv(override=True)
     if not os.getenv("OPENAI_API_KEY") or not os.getenv("ASSISTANT_ID"):
-        return RedirectResponse(url="/setup")
+        return RedirectResponse(url=app.url_path_for("read_setup"))
     
     # Create a new assistant chat thread if no thread ID is provided
     if not thread_id or thread_id == "None" or thread_id == "null":
@@ -75,24 +74,6 @@ async def read_home(request: Request, thread_id: str = None, messages: list = []
             "messages": messages,
             "thread_id": thread_id
         }
-    )
-
-
-# Add new setup route
-@app.get("/setup")
-async def read_setup(request: Request, message: str = None):
-    # Check if assistant ID is missing
-    load_dotenv(override=True)
-    if not os.getenv("OPENAI_API_KEY"):
-        message="OpenAI API key is missing."
-    elif not os.getenv("ASSISTANT_ID"):
-        message="Assistant ID is missing."
-    else:
-        message="All set up!"
-    
-    return templates.TemplateResponse(
-        "setup.html",
-        {"request": request, "message": message}
     )
 
 if __name__ == "__main__":
